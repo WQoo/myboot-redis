255ebc626dca4d63709748ce71130851d919dade
Ranger <642989579@qq.com>
2019-07-03 16:00:10 +0800

    Revert "添加spring boot redis配置，添加redisson配置，添加logback配置"
    This reverts commit 0db07dcf1b8d6f80443d4a05aea9b5a87b84f5fc.

--- src/main/resources/script/limit_quantity.lua
+++
@@ -1,9 +0,0 @@
-local key1 = KEYS[1]
-local key2 = KEYS[2]
-local key3 = KEYS[3]
---- 设置key和value
-redis.call("SET",key1,key2)
---- 设置key的过期时间
-redis.call("EXPIRE",key1,key3)
---- 执行完返回true
-return true
